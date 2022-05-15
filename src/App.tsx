e87b56e3eb12455e516c9297553a38868d4c6186
Luis Martinez <luis@passionatepeople.io>
2022-05-15 10:20:37 +0200

    chore: first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HelloWorld from '@/components/HelloWorld'
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <HelloWorld message="Hello World" />
+    </div>
+  );
+}
+
+export default App;
